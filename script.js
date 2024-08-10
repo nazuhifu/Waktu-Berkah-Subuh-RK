9e94b0e7e047be59515576235bb4d349c7e65edc
Mohammad Nazhiif <86418250+nazuhifu@users.noreply.github.com>
2024-08-10 20:51:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 // Toggle Dark Mode on Button Click
 document.addEventListener("DOMContentLoaded", function () {
     const toggleButton = document.getElementById("dark-mode-toggle");
-    const logoSpan = document.querySelector(".logo span");
-    const body = document.body;
+
     const header = document.querySelector("header");
+    const body = document.body;
+    const logoSpan = document.querySelector(".logo span");
     const navLinks = document.querySelectorAll(".nav-links a");
+    const navLinksDropdown = document.querySelectorAll(".nav-links .dropdown-content a");
+    const navLinksButton = document.querySelectorAll(".nav-links .dark-mode-btn button");
+    const navLinksSvg = document.querySelectorAll(".nav-links .dark-mode-btn button svg");
     const main = document.querySelector("main");
-    const content = document.querySelector(".content");
-    const titleBlocks = document.querySelectorAll(".title-block");
-    const ayat = document.querySelectorAll(".ayat");
-    const taawudz = document.querySelectorAll(".taawudz");
-    const head = document.getElementById("header");
+    const title = document.querySelectorAll(".title");
+
     const lightIcon = document.getElementById("light-icon");
     const darkIcon = document.getElementById("dark-icon");
 
     // Check if dark mode is preferred by user
-    const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    let darkMode = darkModeMediaQuery.matches;
+    let darkMode;
 
-    // Set dark-mode class on body and update icons
-    if (darkMode) {
-        body.classList.add("dark-mode");
-        darkIcon.style.display = "block";
-        lightIcon.style.display = "none";
-    } else {
-        lightIcon.style.display = "block";
-        darkIcon.style.display = "none";
-    }
+    // Set default
+    darkIcon.style.display = "block";
+    lightIcon.style.display = "none";
 
     // Add event listener for toggle button
     toggleButton.addEventListener("click", function () {
@@ -42,20 +36,14 @@ document.addEventListener("DOMContentLoaded", function () {
             darkIcon.style.display = "none";
         }
 
-        main.classList.toggle("dark-mode");
-        head.classList.toggle("dark-mode");
-        content.classList.toggle("dark-mode");
-
-        titleBlocks.forEach((block) => block.classList.toggle("dark-mode"));
-        ayat.forEach((ayat) => ayat.classList.toggle("dark-mode"));
-        taawudz.forEach((text) => text.classList.toggle("dark-mode"));
-
-        // Toggle dark-mode class on body
         body.classList.toggle("dark-mode");
-
-        // Toggle dark mode styles for header, nav links, and logo span
         header.classList.toggle("dark-mode");
-        navLinks.forEach((link) => link.classList.toggle("dark-mode"));
         logoSpan.classList.toggle("dark-mode");
+        navLinks.forEach((link) => link.classList.toggle("dark-mode"));
+        navLinksButton.forEach((link) => link.classList.toggle("dark-mode"));
+        navLinksSvg.forEach((link) => link.classList.toggle("dark-mode"));
+        navLinksDropdown.forEach((drop) => drop.classList.toggle("dark-mode"));
+        main.classList.toggle("dark-mode");
+        title.forEach((block) => block.classList.toggle("dark-mode"));
     });
 });
